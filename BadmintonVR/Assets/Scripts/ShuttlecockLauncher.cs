672b43c11e53495678318ea40d1997e13d15c27b
Alix-Rnt <153102686+Alix-Rnt@users.noreply.github.com>
2026-03-31 14:46:37 -0400

    Racket Grab Management

--- BadmintonVR/Assets/Scripts/ShuttlecockLauncher.cs
+++ BadmintonVR/Assets/Scripts/ShuttlecockLauncher.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class ShuttlecockLauncher : MonoBehaviour
 {
+    [Header("Objects")]
     public GameObject shuttlecockPrefab;
+    public Transform rotatingPart;
     public Transform spawnPoint;
 
+    [Header("Actions")]
+    public float launch;
+
+    [Header("Parameters")]
     public float launchSpeed = 15f;
     public float launchInterval = 2f;
     public float despawnTime = 10f;
@@ -13,6 +19,11 @@ public class ShuttlecockLauncher : MonoBehaviour
     void Start()
     {
         InvokeRepeating("Launch", 1f, launchInterval);
+    }
+
+    void Update()
+    {
+        // rotatingPart.Rotate(0, 20 * Time.deltaTime, 0);
     }
 
     void Launch()
