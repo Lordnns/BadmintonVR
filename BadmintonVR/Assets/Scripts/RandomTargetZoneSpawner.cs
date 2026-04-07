dacc3c015df0b621abb80f465467d761fe4252f5
ESTEBAN MANDJUKOWSKI <emandjukow@etu.uqac.ca>
2026-04-07 15:45:42 -0400

    WIP Target Zone

---
+++ BadmintonVR/Assets/Scripts/RandomTargetZoneSpawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class RandomTargetZoneSpawner : MonoBehaviour
+{
+    [Header("Détails de la zone de spawns possibles")]
+    private Vector3 TopLeftPoint;
+
+    public Vector3 BottomRightPoint;
+
+    public float width;
+    public float height;
+    
+    [Header("Prefab de la target zone")]
+    public GameObject TargetZonePrefab;
+    
+    public float cylinderRadius = 0.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TopLeftPoint = transform.GetChild(0).position;
+        Debug.Log("Top left : " + TopLeftPoint);
+        BottomRightPoint = transform.GetChild(1).position;
+        Debug.Log("Bottom right : " + BottomRightPoint);
+        
+        width = BottomRightPoint.x - TopLeftPoint.x;
+        height = BottomRightPoint.y - TopLeftPoint.y;
+        
+        Instantiate(TargetZonePrefab, TopLeftPoint, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnNewZone()
+    {
+        float x = Random.Range(cylinderRadius,width);
+        float y = Random.Range(cylinderRadius, height);
+        
+        Vector3 targetZonePos = TopLeftPoint + new Vector3(x,0,y);
+        
+        Instantiate(TargetZonePrefab, targetZonePos, Quaternion.identity);
+    }
+}
