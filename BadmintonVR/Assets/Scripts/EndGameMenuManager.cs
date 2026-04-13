50251f532e434cd326d851fcdccad6765d971bfe
Tom <61981635+BTom-GH@users.noreply.github.com>
2026-04-13 18:40:25 -0400

    feat: add score to end game menu

--- BadmintonVR/Assets/Scripts/EndGameMenuManager.cs
+++ BadmintonVR/Assets/Scripts/EndGameMenuManager.cs
@@ -3,13 +3,19 @@ using TMPro;
 
 public class EndGameMenuManager : MonoBehaviour
 {
-
+    public TMP_Text scoreText;
     public TMP_Text timeText;
 
     public void SetTimeText()
     {
         float duration = GameSettings.duration;
         timeText.text = "Time: " + duration.ToString("F2");
+    }
+
+    public void SetScoreText()
+    {
+        float score = GameSettings.score;
+        scoreText.text = "Score: " + score.ToString("F2");
     }
 
     public void Start()
--- BadmintonVR/Assets/Scripts/GameSettings.cs
+++ BadmintonVR/Assets/Scripts/GameSettings.cs
@@ -2,4 +2,5 @@
 {
   public static bool isLeftHanded = false;
   public static float duration = 0.0f;
+  public static float score = 0.0f;
 }
