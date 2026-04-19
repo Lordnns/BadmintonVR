4138d22d2be0388c32c3f9b6ebe35abfc72c152c
Alix-Rnt <153102686+Alix-Rnt@users.noreply.github.com>
2026-04-19 23:24:05 +0200

    Added sine physics

--- BadmintonVR/Assets/Scripts/ShuttlecockData.cs
+++ BadmintonVR/Assets/Scripts/ShuttlecockData.cs
@@ -3,7 +3,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ShuttlecockData", menuName = "Scriptable Objects/ShuttlecockData")]
 public class ShuttlecockData : ScriptableObject
 {
-    public float dragCoefficient = 0.1f;
-    public float initialSpeed = 15f;
+    [Header("Global speed")]
+    [Range(5f, 15f)]
+    public float initialSpeed = 10f;
+
+    [Header("Unity Physics")]
     public float shuttlecockMass = 0.005f;
+    public float dragCoefficient = 0.1f;
+
+    [Header("Sine Physics")]
+    [Range(0f, 10f)]
+    public float height = 2.0f;
+
+    [Header("Aerodynamism")]
+    public float liftCoefficient = 0.1f;
+    public float autoRotateStrength = 8f;
+
+    [Header("Stabilisation")]
+    public float angularDamping = 3f;
+
+    [Header("Others")]
+    public float despawnTime = 10f;
 }
